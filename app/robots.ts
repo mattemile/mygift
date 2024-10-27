67805ff70cc1292cdd68cdce5e73f85c3be651da
mattemile <milevamatteo@gmail.com>
2024-10-27 11:36:10 +0100

    fix a

--- app/robots.ts
+++ app/robots.ts
@@ -11,7 +11,7 @@ export default function robots(): MetadataRoute.Robots {
         userAgent: '*'
       }
     ],
-    sitemap: `${baseUrl}/sitemap.xml`,
+    //sitemap: `${baseUrl}/sitemap.xml`,
     host: baseUrl
   }
 }
